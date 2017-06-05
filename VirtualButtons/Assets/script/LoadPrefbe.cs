41f35f364b2194edc1fca2ed989b5ea4bee214b0
tantanbei <tan1115@outlook.com>
2017-06-05 23:30:25 +0800

    MOD load the glass prefab file on c-sharp code

---
+++ VirtualButtons/Assets/script/LoadPrefbe.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadPrefbe : MonoBehaviour {
+
+	public string url;
+	void Start () {
+		GameObject abc = Resources.Load("glass") as GameObject;
+
+		abc = Instantiate (abc);
+ 
+		GameObject mUICanvas = GameObject.Find("ImageTaget");  
+		abc.transform.parent = mUICanvas.transform;  
+	}
+
+	void Update () {
+
+	}    
+}
